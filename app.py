193911f36ac55c4060e530d84457a32a21ba338f
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2023-03-11 23:27:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-model = joblib.load('model_1.pkl')
+model = joblib.load('https://github.com/Adefebrian/nerforest/blob/main/model_1.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
