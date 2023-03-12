561c4c5ebaae898ae41ae6bee8936f2c83494a8f
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2023-03-12 15:42:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,124 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+import twint
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
+# Load model
+model = tf.keras.models.load_model('model.h5')
+
+# Define label mapping
+label_mapping = {
+    0: 'Bukan kebakaran',
+    1: 'Kebakaran',
+    2: 'Penanganan'
+}
+
+# Initialize Twint config
+c = twint.Config()
+
+# Set Twint search query
+c.Search = 'kebakaran hutan'
+
+# Set Twint callback function
+def on_tweet_callback(tweet):
+    # Get tweet text
+    tweet_text = tweet.tweet
+
+    # Preprocess tweet text
+    tweet_text = preprocess_text(tweet_text)
+
+    # Convert tweet text to vector
+    tweet_vector = text_to_vector(tweet_text)
+
+    # Make prediction using model
+    prediction = model.predict(tweet_vector)
+
+    # Get predicted label
+    predicted_label = np.argmax(prediction)
+
+    # Get label name from mapping
+    label_name = label_mapping[predicted_label]
+
+    # Construct response JSON
+    response = {
+        'post': tweet_text,
+        'label': label_name
+    }
+
+    # Send response JSON
+    send_response(response)
+
+# Set Twint callback
+c.Callback = on_tweet_callback
+
+# Set Twint limit
+c.Limit = 10
+
+# Set Twint hide output
+c.Hide_output = True
+
+# Start Twint search
+twint.run.Search(c)
+
+# Initialize Flask app
 app = Flask(__name__)
 
-# Load trained model
-model = load_model('model.h5')
+# Define prediction function
+def predict_label(tweet_text):
+    # Preprocess tweet text
+    tweet_text = preprocess_text(tweet_text)
 
-# Initialize tokenizer with vocabulary size of 10000
-tokenizer = Tokenizer(num_words=10000)
+    # Convert tweet text to vector
+    tweet_vector = text_to_vector(tweet_text)
 
+    # Make prediction using model
+    prediction = model.predict(tweet_vector)
+
+    # Get predicted label
+    predicted_label = np.argmax(prediction)
+
+    # Get label name from mapping
+    label_name = label_mapping[predicted_label]
+
+    return label_name
+
+# Define preprocessing function
+def preprocess_text(text):
+    # Implement your text preprocessing logic here
+    # ...
+    return text
+
+# Define text to vector function
+def text_to_vector(text):
+    # Implement your text to vector conversion logic here
+    # ...
+    return vector
+
+# Define send response function
+def send_response(response):
+    # Implement your response sending logic here
+    # ...
+    print(response)
+
+# Define endpoint for predicting label
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get data from request
+    # Get request data
     data = request.json
-    tweet = data['tweet']
 
-    # Tokenize text
-    tokenizer.fit_on_texts(tweet)
-    sequences = tokenizer.texts_to_sequences(tweet)
+    # Get tweet text from request data
+    tweet_text = data['tweet']
 
-    # Pad sequences
-    sequences_padded = pad_sequences(sequences, maxlen=100, padding='post', truncating='post')
+    # Predict label using model
+    predicted_label = predict_label(tweet_text)
 
-    # Make predictions
-    predictions = model.predict(sequences_padded)
+    # Construct response JSON
+    response = {
+        'post': tweet_text,
+        'label': predicted_label
+    }
 
-    # Convert predictions to labels
-    labels = np.argmax(predictions, axis=1)
-
-    # Return predictions as JSON
-    return jsonify({'predictions': labels.tolist()})
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
