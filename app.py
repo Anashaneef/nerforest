d15a5d0a1f31a95a628163389e4201b799cefb63
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2023-03-12 15:20:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-import tweepy
-from tensorflow.keras.models import load_model
 import os
+import re
+from flask import Flask, jsonify, request
+from twitter_scraper import get_tweets
 
-# Load model
-model_path = 'model.h5'
-model = load_model(model_path)
-
-# Set up Twitter API credentials
-consumer_key = 'u9D8IF1iOVWaoRy24598GyRq9'
-consumer_secret = 'WYOe2e2Nkb2Q0EIvqK0spsOfdsiNk9lkzTkzvigUTn1PK5wrzm'
-access_token = '882952289450774529-NIYPtaMdpiJCbAVSINxORsjS0Ba2GaO'
-access_token_secret = 'LWuTR82dsTKdoK6AZ4j9SdDNbJhHDHlyBUDGO2fNLZxKe'
-
-# Authenticate with Twitter API
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-# Initialize Tweepy API client
-api = tweepy.API(auth)
+app = Flask(__name__)
 
 # Define keyword to search for
 keyword = 'kebakaran hutan'
@@ -30,27 +16,42 @@ def predict_label(text):
         1: 'kebakaran',
         2: 'penanganan'
     }
-    
+
+    # Preprocess text
+    text = text.lower()
+    text = re.sub(r'http\S+', '', text)  # Remove URLs
+    text = re.sub(r'[^\w\s]', '', text)  # Remove punctuation
+
     # Predict label
-    label = model.predict([text])[0]
-    
+    # Replace the following line with your model prediction code
+    label = 0
+
     # Map label index to label text
-    label_text = label_map[label.argmax()]
-    
+    label_text = label_map[label]
+
     return label_text
 
 # Define function to process tweet and predict label
 def process_tweet(tweet):
-    text = tweet.text
+    text = tweet['text']
     label = predict_label(text)
     print(f'Tweet: {text}')
     print(f'Label: {label}')
     print()
 
+# Define endpoint for processing new tweets
+@app.route('/process-tweets', methods=['POST'])
+def process_tweets():
+    tweets = request.get_json()['tweets']
+
+    for tweet in tweets:
+        process_tweet(tweet)
+
+    return jsonify({'message': 'Tweets processed successfully.'})
+
 # Stream tweets with keyword and process them in real time
-stream = tweepy.Stream(auth=api.auth, listener=None)
-stream.filter(track=[keyword], is_async=True, languages=['id'])
+for tweet in get_tweets(keyword, pages=1):
+    process_tweet(tweet)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
