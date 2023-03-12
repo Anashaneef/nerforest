62d38cfcba05eec7aec4cf92bd99cddf41ffa464
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2023-03-12 17:13:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,63 +13,18 @@ label_mapping = {
     2: 'Penanganan'
 }
 
-# Initialize Twint config
-c = twint.Config()
-
-# Set Twint search query
-c.Search = 'kebakaran hutan'
-
-# Set Twint callback function
-def on_tweet_callback(tweet):
-    # Get tweet text
-    tweet_text = tweet.tweet
-
-    # Preprocess tweet text
-    tweet_text = preprocess_text(tweet_text)
-
-    # Convert tweet text to vector
-    tweet_vector = text_to_vector(tweet_text)
-
-    # Make prediction using model
-    prediction = model.predict(tweet_vector)
-
-    # Get predicted label
-    predicted_label = np.argmax(prediction)
-
-    # Get label name from mapping
-    label_name = label_mapping[predicted_label]
-
-    # Construct response JSON
-    response = {
-        'post': tweet_text,
-        'label': label_name
-    }
-
-    # Send response JSON
-    send_response(response)
-
-# Set Twint callback
-c.Callback = on_tweet_callback
-
-# Set Twint limit
-c.Limit = 10
-
-# Set Twint hide output
-c.Hide_output = True
-
-# Start Twint search
-twint.run.Search(c)
-
 # Initialize Flask app
 app = Flask(__name__)
 
 # Define prediction function
 def predict_label(tweet_text):
-    # Preprocess tweet text
-    tweet_text = preprocess_text(tweet_text)
-
-    # Convert tweet text to vector
-    tweet_vector = text_to_vector(tweet_text)
+    # Implement your text preprocessing logic here
+    # ...
+    preprocessed_text = preprocess_text(tweet_text)
+    
+    # Implement your text to vector conversion logic here
+    # ...
+    tweet_vector = text_to_vector(preprocessed_text)
 
     # Make prediction using model
     prediction = model.predict(tweet_vector)
@@ -99,6 +54,39 @@ def send_response(response):
     # Implement your response sending logic here
     # ...
     print(response)
+
+# Set Twint search query
+c = twint.Config()
+c.Search = 'kebakaran hutan'
+
+# Set Twint callback function
+def on_tweet_callback(tweet):
+    # Get tweet text
+    tweet_text = tweet.tweet
+
+    # Predict label using model
+    predicted_label = predict_label(tweet_text)
+
+    # Construct response JSON
+    response = {
+        'post': tweet_text,
+        'label': predicted_label
+    }
+
+    # Send response JSON
+    send_response(response)
+
+# Set Twint callback
+c.Callback = on_tweet_callback
+
+# Set Twint limit
+c.Limit = 10
+
+# Set Twint hide output
+c.Hide_output = True
+
+# Start Twint search
+twint.run.Search(c)
 
 # Define endpoint for predicting label
 @app.route('/predict', methods=['POST'])
